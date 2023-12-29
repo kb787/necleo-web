8fc67d1d5b35b7ccca6375101748130517754157
Karan <karanbhanu788@gmail.com>
2023-12-29 22:26:29 +0530

    routing-changes

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
        <BrowserRouter>
             <Routes>
                  <Route path = "/" element = {<Dashboard/>} />
+                 <Route path = "/necleo-web" element = {<Dashboard/>} />
             </Routes>
        </BrowserRouter>
     </div>
